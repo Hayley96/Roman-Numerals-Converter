7f210d299990d492b23336f7101611fb5632c76c
Hayley96 <h.jonas@outlook.com>
2022-05-17 00:06:57 +0100

    More whitespace removed

--- RomanNumeralsConverter/Classes/RunConversion.cs
+++ RomanNumeralsConverter/Classes/RunConversion.cs
@@ -7,4 +7,3 @@ public class RunConversion
         ConvertedValue = dict.NumDictionary[ValueToConvert];
     }
 }
-
--- RomanNumeralsConverter/Classes/SetDictionary.cs
+++ RomanNumeralsConverter/Classes/SetDictionary.cs
@@ -20,8 +20,4 @@ public class SetDictionary
     public SetDictionary()
     {
     }
-
-
-
-
 }
